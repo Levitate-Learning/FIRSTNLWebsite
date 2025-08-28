0aef10884403f5c9b3d1f6af179dfe8257c1b787
Troake <angtroake@mun.ca>
2025-08-28 11:35:56 -0230

    last fixes before sending team email

--- pages.config.js
+++ pages.config.js
@@ -33,6 +33,6 @@ module.exports = [
     {template: "base.ejs", output: "ftc.html", data: {page: "ftc/ftc"}},
     {template: "base.ejs", output: "ftc/startateam.html", data: {page: "ftc/start_a_team"}},
     {template: "base.ejs", output: "ftc/robot.html", data: {page: "ftc/robot_resources"}},
-    {template: "base.ejs", output: "ftc/coding.html", data: {page: "ftc/robot_resources"}},
+    {template: "base.ejs", output: "ftc/coding.html", data: {page: "ftc/programming_resources"}},
     {template: "base.ejs", output: "sponsor.html", data: {page: "sponsor"}}
 ]
